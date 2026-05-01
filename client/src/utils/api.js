7f1ac884bcec83a3553933de502b5ffb41ed8e64
Shubhsirat Kaur <shubh130105@gmail.com>
2026-05-01 10:32:54 +0530

    fix api url for production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+const BASE_URL = import.meta.env.VITE_API_URL || "/api";
+
+const api = axios.create({ 
+  baseURL: BASE_URL 
 });
 
 api.interceptors.request.use((config) => {
