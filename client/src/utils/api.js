49011c6c52195cd7876b52b2b7db1ad891465601
Shubhsirat Kaur <shubh130105@gmail.com>
2026-05-16 12:58:31 +0530

    allow all origins for public access

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "/api";
-
-const api = axios.create({ 
-  baseURL: BASE_URL 
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "/api",
 });
 
 api.interceptors.request.use((config) => {
